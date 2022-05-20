95c0e8a3a4911e8ccb2404776dc4ebb3485f5a2a
Paulo Roberto Moreira Mendes <prmoreira.dev@gmail.com>
2022-05-20 15:30:41 -0300

    Create 1.4_estrutura_sequencial.py

---
+++ 1.4_estrutura_sequencial.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+b1 = float(input('Nota do 1º Bimestre: '))
+b2 = float(input('Nota do 2º Bimestre: '))
+b3 = float(input('Nota do 3º Bimestre: '))
+b4 = float(input('Nota do 4º Bimestre: '))
+media = (b1 + b2 + b3 + b4)/4
+print(f'A média final das Notas de 4 semestres foi: {media}')
