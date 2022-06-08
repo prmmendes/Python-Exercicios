8110f9df052e47d0557907bdc8ea8f6c91f02d4f
Paulo Roberto Moreira Mendes <prmoreira.dev@gmail.com>
2022-06-08 15:06:24 -0300

    Update 5.01_funcoes.py

--- 5.01_funcoes.py
+++ 5.01_funcoes.py
@@ -4,12 +4,13 @@
 # 3     3       3
 # .........
 # n     n       n       n       n
+#para um n informado pelo usuário. Use a função que receba um valor n inteiro e imprima até a n-ésima linha.
 
 def imprimirPiramide(linhas):
     for c in range(1, linhas + 1):
         cont = 1
         while True:
-            print(c, end='    ')
+            print(f'{c:>2}', end='    ')
             if cont == c:
                 break
             cont += 1
