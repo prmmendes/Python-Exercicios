64edbf7a36679cb88d9c5223dfa5fcb9f36bb087
Paulo Roberto Moreira Mendes <prmoreira.dev@gmail.com>
2023-04-04 17:56:03 -0300

    Create 4.23_listas.py

---
+++ 4.23_listas.py
@@ -0,0 +1,17 @@
+#A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos.
+#Para tentar resolver este problema, o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários, e
+#identificar os usuários com maior espaço ocupado. Através de um programa, baixado na Internet, ele conseguiu gerar
+#o seguinte arquivo, chamado "usuários.txt":
+# Alexandre         456123789
+# Anderson          1245698456
+# Antônio           123456456
+# Carlos            91257581
+# Cesar             987458
+# Rosemary          789456125
+#Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste arquivo chamado "relatório.txt",
+#no seguinte formato:
+#(consultar formato na lista de exercícios)
+#O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários, de forma a
+#agilizar a execução do programa. A conversão do espaço ocupado em disco, de bytes para megabytes deverá ser feita
+#através de uma função, que será chamada pelo programa principal.
+
